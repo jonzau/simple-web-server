e3aa32c5fb9a5dc311e9a00e481bfa52275d8d22
Jonathan Z <izauritzb@gmail.com>
2022-04-12 18:18:44 -0400

    Use path variable for web directory.

--- server.js
+++ server.js
@@ -2,14 +2,14 @@ const express = require('express');
 const path = require('path');
 
 const PORT = process.env.PORT || 8080;
+const PATH = '/web';
 
 const server = express();
-server.use(express.static(__dirname + '/web'));
+server.use(express.static(__dirname + PATH));
 
-server.get('/*', function(req,res) {
-  console.log(path.join(__dirname + '/web/index.html'));
-  res.sendFile(path.join(__dirname + '/web/index.html'), (err) => {
-    res.sendFile(__dirname + '/web/404.html', (err) => {
+server.get('/*', function(req, res) {
+  res.sendFile(path.join(__dirname + PATH + '/index.html'), (err) => {
+    res.sendFile(__dirname + PATH + '/404.html', (err) => {
       res.sendFile(__dirname + '/404.html');
     });
   });
